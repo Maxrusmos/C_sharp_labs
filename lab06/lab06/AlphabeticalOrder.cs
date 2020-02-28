6125c73cdb497b4bdcf6c6426892a57d030b631f
Maxim Vorobyev <vmaxim7777@gmail.com>
2020-02-28 22:25:00 +0300

    lab06

---
+++ lab06/lab06/AlphabeticalOrder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AlphabeticalOrder {
+  public class AlphabeticalOrder {
+    public static void SortAlphabeticalOrder(string[] mainStr, out string AlphabeticalOrderStr, out string lastLetterStr) {
+      lastLetterStr = "";
+      AlphabeticalOrderStr = "";
+      for (int i = 0; i < mainStr.Length - 1; i++) {
+        for (int j = 0; j < mainStr.Length - i - 1; j++) {
+          if (mainStr[j + 1].CompareTo(mainStr[j]) < 0) {
+            var temp = mainStr[j + 1];
+            mainStr[j + 1] = mainStr[j];
+            mainStr[j] = temp;
+          }
+        }
+      }
+
+      foreach (string str in mainStr) {
+        lastLetterStr = lastLetterStr + str[str.Length - 1];
+        AlphabeticalOrderStr = AlphabeticalOrderStr + str + " ";
+      }
+    }
+  }
+}
---
+++ lab06/lab06/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AlphabeticalOrder;
+
+namespace lab06 {
+  class Program {
+    static void Main(string[] args) {
+      Console.ForegroundColor = ConsoleColor.Yellow;
+      Console.Write("Введите слова, которые хотите отсортировать по алфавитному порядку: ");
+      var mainStr = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+      var AlphabeticalOrderStr = "";
+      var lastLetterStr = "";
+
+      AlphabeticalOrder.AlphabeticalOrder.SortAlphabeticalOrder(mainStr, out AlphabeticalOrderStr, out lastLetterStr);
+
+      Console.WriteLine();
+      Console.ForegroundColor = ConsoleColor.Cyan;
+      Console.Write("Слова в алфавитном порядке: " + AlphabeticalOrderStr);
+      Console.WriteLine("\n");
+      Console.ForegroundColor = ConsoleColor.Cyan;
+      Console.Write("Слово из последних символов: " + lastLetterStr);
+      Console.WriteLine("\n");
+    }
+  }
+}

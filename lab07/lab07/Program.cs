7ef20af48b32c03b38e8efc215d48403b880a332
Maxim Vorobyev <vmaxim7777@gmail.com>
2020-02-28 21:11:15 +0300

    add lab07

---
+++ lab07/lab07/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using StrSort;
+
+namespace lab07 {
+  class Program {
+    static void Main(string[] args) {
+      Console.ForegroundColor = ConsoleColor.Yellow;
+      Console.Write("Введите строку: ");
+      var str = Console.ReadLine();
+      var strBuild = new StringBuilder(str);
+
+      Console.ForegroundColor = ConsoleColor.Cyan;
+      Console.WriteLine("Отсортированная строка: " + StrSort.StrSort.AscendingOrder(strBuild) + "/n");      
+    }
+  }
+}
---
+++ lab07/lab07/StrSort.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace StrSort {
+  public class StrSort {
+    public static StringBuilder AscendingOrder(StringBuilder str) {
+      for (int i = 0; i < str.Length - 1; i++) {
+        for (int j = 0; j < str.Length - i - 1; j++) {
+          if (str[j + 1] < str[j]) {
+            var temp = str[j + 1];
+            str[j + 1] = str[j];
+            str[j] = temp;
+          }
+        }
+      }
+      return str;
+    }
+  }
+}

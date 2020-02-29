cbdd3f53e62c56e4c165925bb86cbebcd39cb89f
Maxim Vorobyev <vmaxim7777@gmail.com>
2020-02-29 14:13:32 +0300

    add lab02

---
+++ lab02/lab02/PiELn2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PiELn2 {
+  public class PiELn2 {
+    private static decimal Fact(int n) {
+      decimal factorial = 1;
+
+      for (int i = 2; i <= n; i++) {
+        factorial = factorial * i;
+      }
+      return factorial;
+    }
+
+    public static void calc(out decimal pi, out decimal e, out decimal ln2) {
+      pi = 0;
+      e = 0;
+      ln2 = 0;
+
+      for (int k = 0; k < 15; k++) {
+        pi = pi + (1 / (decimal)Math.Pow(16, k)) * (decimal.Divide(4, 8 * k + 1) - decimal.Divide(2, 8 * k + 4) - decimal.Divide(1, 8 * k + 5) - decimal.Divide(1, 8 * k + 6));
+      }
+      pi = Math.Round(pi, 15);
+
+      for (int n = 1; n <= 18; n++) {
+        e = decimal.Add(e, decimal.Divide(1, Fact(n)));
+      }
+      e = Math.Round(e + 1, 15);
+
+      for (int n = 2; n < 50; n++) {
+        ln2 = decimal.Add(ln2, decimal.Divide(1, decimal.Multiply(n, (decimal)Math.Pow(2, n))));
+      }
+      ln2 = Math.Round(decimal.Add(ln2, decimal.Divide(1, 2)), 15);
+    }
+  }
+}
---
+++ lab02/lab02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using PiELn2;
+
+namespace lab02 {
+  class Program {
+    static void Main(string[] args) {
+      decimal pi = 0;
+      decimal e = 0;
+      decimal ln2 = 0;
+
+      PiELn2.PiELn2.calc(out pi, out e, out ln2);
+      Console.WriteLine(" Pi = " + pi);
+      Console.WriteLine("  E = " + e);
+      Console.WriteLine("ln2 = " + ln2);
+    }
+  }
+}

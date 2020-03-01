23fbfec885f0c3ba979140421d19d2f985f5cf05
Maxim Vorobyev <vmaxim7777@gmail.com>
2020-03-01 20:53:07 +0300

    fix

--- lab05/lab05/FractionalPartConvert.cs
+++ lab05/lab05/FractionalPartConvert.cs
@@ -1,11 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using WholePartConvert;
 
 namespace FractionalPartConvert {
   public static class FractionalPartConvert {
     private static string SearchPeriod(string fractionalPart) {
+      Dictionary<char, int> periodDict = new Dictionary<char, int>();
+      var periodCount = 0;
+      var count = 0;
+      var period = "";
 
-      return "";
+      for (int i = 0; i < fractionalPart.Length; i++) {
+        for (int j = 0; j < fractionalPart.Length; j++) {
+          if (fractionalPart[i] == fractionalPart[j])
+            count++;
+        }
+
+        if (!periodDict.ContainsKey(fractionalPart[i])) {
+          periodDict.Add(fractionalPart[i], count);
+        }
+
+        if (periodCount < count) {
+          periodCount = count;
+        }
+        count = 0;
+      }
+
+      var notPeriod = "";
+      foreach (KeyValuePair<char, int> kvp in periodDict.Where(f => f.Value < (-periodCount / 2.0) - periodCount)) {
+        Console.WriteLine(kvp.Key + " : " + kvp.Value);
+        notPeriod = notPeriod + kvp.Key;
+      }
+
+      foreach (KeyValuePair<char, int> kvp in periodDict.Where(f => (f.Value == periodCount) || (f.Value == periodCount - 1))) {
+        Console.WriteLine(kvp.Key + " : " + kvp.Value);
+        period = period + kvp.Key;
+      }
+      Console.WriteLine(notPeriod + "(" + period + ")");
+      return notPeriod + "(" + period + ")";
     }
 
     public static string FToNS(double fractionalNumber, int numBase) {
@@ -13,12 +46,17 @@ namespace FractionalPartConvert {
       var result = "";
 
       int discharge = 0;
-      while (discharge != 14 || fractionalNumber == 0.0) {
+      while (discharge != 100) {
         tmpNum = fractionalNumber * numBase;
-        result = result + WholePartConvert.WholePartConvert.correct(int.Parse(Convert.ToString(tmpNum).Split(',')[0])); //result + целая часть
-        fractionalNumber = double.Parse("0," + Convert.ToString(tmpNum).Split(',')[1]);
+        result = result + WholePartConvert.WholePartConvert.correct(int.Parse(Convert.ToString(tmpNum).Split(',')[0]));
+        if (Convert.ToString(tmpNum).Split(',').Length == 1) {
+          break;
+        } else {
+          fractionalNumber = double.Parse("0," + Convert.ToString(tmpNum).Split(',')[1]);
+        }
         discharge++;
       }
+      SearchPeriod(result);
       return result;
     }
   }

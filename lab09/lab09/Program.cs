5043311f0c7629a0dcce4058f58fb078328d0fb0
Maxim Vorobyev <vmaxim7777@gmail.com>
2020-03-02 19:31:39 +0300

    add lab09

---
+++ lab09/lab09/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Dichotomy;
+
+namespace lab09 {
+  class Program {
+    static void Main(string[] args) {
+      Dichotomy.Dichotomy.deligateEquation GetEq = new Dichotomy.Dichotomy.deligateEquation(Dichotomy.Dichotomy.EquationGet);
+      Console.WriteLine("Введите границы интервала.");
+      Console.Write("Введите нижнюю границу: ");
+      var lowerBorder = Console.ReadLine();
+
+      if (!double.TryParse(lowerBorder, out double number1)) {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Нижняя граница не является числом");
+        System.Environment.Exit(1);
+      }
+
+      Console.Write("Введите верхнюю границу: ");
+      var upperBorder = Console.ReadLine();
+
+      if (!double.TryParse(lowerBorder, out double number2)) {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Верхняя граница не является числом");
+        System.Environment.Exit(2);
+      }
+
+      Console.Write("Введите точность: ");
+      var accuracy = Console.ReadLine();
+
+      if (!double.TryParse(lowerBorder, out double number3)) {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Введенная точность не является числом");
+        System.Environment.Exit(3);
+      }
+
+      Console.WriteLine(Dichotomy.Dichotomy.DichotomySolve(double.Parse(lowerBorder), 
+        double.Parse(upperBorder), GetEq, double.Parse(accuracy))); 
+    }
+  }
+}
---
+++ lab09/lab09/dichotomy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Dichotomy {
+  public static class Dichotomy {
+    public delegate double deligateEquation(double x);
+    public static string DichotomySolve(double borderFirst, double borderSecond, deligateEquation GetEq, double accuracy) {
+      var min = borderFirst;
+      var max = borderSecond;
+      var intervalLength = borderSecond - borderFirst;
+      var err = intervalLength;
+      var x = 0.0;
+      while (err > accuracy && GetEq(x) != 0) {
+        x = (min + max) / 2;
+        if (GetEq(min) * GetEq(x) < 0) {
+          max = x;
+        } else if (GetEq(x) * GetEq(max) < 0) {
+          min = x;
+        }
+        err = (max - min) / intervalLength;
+      }
+      return Convert.ToString(x);
+    }
+
+    public static double EquationGet(double x){
+      return Math.Cos(x) - x;
+    }
+  }
+}

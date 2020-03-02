e982705a85aa517642c3780da91db138e75aa884
Maxim Vorobyev <vmaxim7777@gmail.com>
2020-03-02 21:01:49 +0300

    add lab10

---
+++ lab10/lab10/Matrix.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab10 {
+  class Matrix {
+    private int n;
+    private int[,] mass;
+
+    // Создаем конструкторы матрицы
+    public Matrix() { }
+    public int N {
+      get {
+        return n;
+      }
+
+      set {
+        if (value > 0) {
+          n = value;
+        }
+      }
+    }
+
+    // Задаем аксессоры для работы с полями вне класса Matrix
+    public Matrix(int n) {
+      this.n = n;
+      mass = new int[this.n, this.n];
+    }
+
+    public int this[int i, int j] {
+      get {
+        return mass[i, j];
+      }
+      set {
+        mass[i, j] = value;
+      }
+    }
+
+    // Ввод матрицы с клавиатуры
+    public void WriteMatrix() {
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          Console.WriteLine("Введите элемент матрицы " + i + 1 + " : " + j + 1);
+          mass[i, j] = Convert.ToInt32(Console.ReadLine());
+        }
+      }
+    }
+
+    // Вывод матрицы
+    public void ReadMatrix() {
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          Console.Write(mass[i, j] + "\t");
+        }
+        Console.WriteLine();
+      }
+    }
+
+    // Умножение матрицы А на число
+    public static Matrix compositionWhithNum(Matrix a, int num) {
+      Matrix resMass = new Matrix(a.N);
+      for (int i = 0; i < a.N; i++) {
+        for (int j = 0; j < a.N; j++) {
+          resMass[i, j] = a[i, j] * num;
+        }
+      }
+      return resMass;
+    }
+
+    // Умножение матрицы А на матрицу Б
+    public static Matrix compositionMatrix(Matrix a, Matrix b) {
+      Matrix resMass = new Matrix(a.N);
+      for (int i = 0; i < a.N; i++)
+        for (int j = 0; j < b.N; j++)
+          for (int k = 0; k < b.N; k++)
+            resMass[i, j] += a[i, k] * b[k, j];
+
+      return resMass;
+    }
+
+    // Метод вычитания матрицы Б из матрицы А
+    public static Matrix differenceMatrix(Matrix a, Matrix b) {
+      Matrix resMass = new Matrix(a.N);
+      for (int i = 0; i < a.N; i++) {
+        for (int j = 0; j < b.N; j++) {
+          resMass[i, j] = a[i, j] - b[i, j];
+        }
+      }
+      return resMass;
+    }
+
+    // Сумма матриц
+    public static Matrix SumMatrix(Matrix a, Matrix b) {
+      Matrix resMass = new Matrix(a.N);
+      for (int i = 0; i < a.N; i++) {
+        for (int j = 0; j < b.N; j++) {
+          resMass[i, j] = a[i, j] + b[i, j];
+        }
+      }
+      return resMass;
+    }
+
+    //транспоирование
+    public static Matrix transpositionMatrix(Matrix a) {
+      Matrix resMass = new Matrix(a.N);
+      for (int i = 0; i < a.N; i++) {
+        for (int j = 0; j < a.N; j++) {
+          resMass[i, j] = a[j, i];
+        }
+      }
+      return resMass;
+    }
+
+    ~Matrix() { }
+  }
+}
---
+++ lab10/lab10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using lab10;
+
+namespace lab10 {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Введите размерность матрицы: ");
+      int nn = Convert.ToInt32(Console.ReadLine());
+      // Инициализация
+      Matrix mass1 = new Matrix(nn);
+      Matrix mass2 = new Matrix(nn);
+      Matrix mass3 = new Matrix(nn);
+      Matrix mass4 = new Matrix(nn);
+      Matrix mass5 = new Matrix(nn);
+      Matrix mass6 = new Matrix(nn);
+      Matrix mass7 = new Matrix(nn);
+      Matrix mass8 = new Matrix(nn);
+
+      Console.WriteLine("ввод Матрица А: ");
+      mass1.WriteMatrix();
+      Console.WriteLine("Ввод Матрица B: ");
+      mass2.WriteMatrix();
+
+      Console.WriteLine("Матрица А: ");
+      mass1.ReadMatrix();
+      Console.WriteLine();
+      Console.WriteLine("Матрица В: ");
+      Console.WriteLine();
+      mass2.ReadMatrix();
+
+      Console.WriteLine("Транспонированная матрица A");
+      mass7 = Matrix.transpositionMatrix(mass1);
+      mass7.ReadMatrix();
+    }
+  }
+}

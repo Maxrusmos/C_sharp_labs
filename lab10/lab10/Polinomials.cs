8f4afe5f027bcbdd248b5decdd9f5db85ac85907
Maxim Vorobyev <vmaxim7777@gmail.com>
2020-03-04 18:02:22 +0300

    fix

---
+++ lab10/lab10/Polinomials.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolynomOp {
+  class Polynomials {
+    double[] koef; //массив коэф-ов
+    int step; //значение степени полинома
+
+    public Polynomials(double[] k, int s) {
+      koef = k;
+      step = s;
+    }
+
+    //сложение полиномов
+    public static Polynomials operator +(Polynomials A, Polynomials B) {
+      int D1 = A.step;
+      double[] M1 = new double[D1 + 1];
+      Polynomials C = new Polynomials(M1, D1);
+      for (int i = 0; i < A.step + 1; i++) {
+        C.koef[i] = A.koef[i] + B.koef[i];
+      }
+      return C;
+    }
+
+
+    //вычитание полиномов
+    public static Polynomials operator -(Polynomials A, Polynomials B) {
+      int D1 = A.step;
+      double[] M1 = new double[D1 + 1];
+      Polynomials C = new Polynomials(M1, D1);
+      for (int i = 0; i < A.step + 1; i++) {
+        C.koef[i] = A.koef[i] - B.koef[i];
+      }
+      return C;
+    }
+
+
+    //умножение полиномов
+    public static Polynomials operator *(Polynomials A, Polynomials B) {
+      int D1 = A.step;
+      double[] M1 = new double[D1 + 1];
+      Polynomials C = new Polynomials(M1, D1);
+      for (int i = 0; i < A.step + 1; i++) {
+        C.koef[i] = A.koef[i] * B.koef[i];
+      }
+      return C;
+    }
+
+    //вывод полинома
+    public void show() {
+      for (int i = 0; i < step; i++) {
+        Console.Write("+" + koef[i] + "x^" + (step - i));
+      }
+    }
+  }
+}
+

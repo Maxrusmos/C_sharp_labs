6c439be7f1c4ba36ad88d637fae3e6f3120c7ef0
Maxim Vorobyev <vmaxim7777@gmail.com>
2020-03-05 21:58:46 +0300

    fix

--- lab09/lab09/Program.cs
+++ lab09/lab09/Program.cs
@@ -4,7 +4,7 @@ using Dichotomy;
 namespace lab09 {
   class Program {
     static void Main(string[] args) {
-      Dichotomy.Dichotomy.deligateEquation GetEq = new Dichotomy.Dichotomy.deligateEquation(Dichotomy.Dichotomy.EquationGet);
+      Dichotomy.Dichotomy.delegateEquation GetEq = new Dichotomy.Dichotomy.delegateEquation(Dichotomy.Dichotomy.EquationGet);
       Console.WriteLine("Введите границы интервала.");
       Console.Write("Введите нижнюю границу: ");
       var lowerBorder = Console.ReadLine();
@@ -12,7 +12,7 @@ namespace lab09 {
       if (!double.TryParse(lowerBorder, out double number1)) {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Нижняя граница не является числом");
-        System.Environment.Exit(1);
+        Environment.Exit(1);
       }
 
       Console.Write("Введите верхнюю границу: ");
@@ -21,16 +21,22 @@ namespace lab09 {
       if (!double.TryParse(lowerBorder, out double number2)) {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Верхняя граница не является числом");
-        System.Environment.Exit(2);
+        Environment.Exit(2);
+      }
+
+      if (double.Parse(lowerBorder) > double.Parse(upperBorder)) {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Верхняя граница оказалась больше чем нижняя");
+        Environment.Exit(3);
       }
 
       Console.Write("Введите точность: ");
       var accuracy = Console.ReadLine();
 
-      if (!double.TryParse(lowerBorder, out double number3)) {
+      if (!double.TryParse(accuracy, out double number3)) {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Введенная точность не является числом");
-        System.Environment.Exit(3);
+        Environment.Exit(4);
       }
 
       Console.WriteLine(Dichotomy.Dichotomy.DichotomySolve(double.Parse(lowerBorder), 
--- lab09/lab09/dichotomy.cs
+++ lab09/lab09/dichotomy.cs
@@ -2,27 +2,34 @@
 
 namespace Dichotomy {
   public static class Dichotomy {
-    public delegate double deligateEquation(double x);
-    public static string DichotomySolve(double borderFirst, double borderSecond, deligateEquation GetEq, double accuracy) {
+    public delegate double delegateEquation(double x);
+    private const double Eps = 1e-14;
+
+    public static string DichotomySolve(double borderFirst, double borderSecond, delegateEquation GetEq, double accuracy) {
       var min = borderFirst;
       var max = borderSecond;
       var intervalLength = borderSecond - borderFirst;
       var err = intervalLength;
       var x = 0.0;
-      while (err > accuracy && GetEq(x) != 0) {
-        x = (min + max) / 2;
-        if (GetEq(min) * GetEq(x) < 0) {
-          max = x;
-        } else if (GetEq(x) * GetEq(max) < 0) {
-          min = x;
+
+        while (err > accuracy && GetEq(x) != 0) {
+          x = (min + max) / 2;
+          if (GetEq(min) * GetEq(x) < 0) {
+            max = x;
+          } else if (GetEq(x) * GetEq(max) < 0) {
+            min = x;
+          } else if (GetEq(min) * GetEq(max) < 0) {
+            Console.WriteLine("ok");
+          } else {
+            Console.WriteLine("kek");
+          }
+          err = (max - min) / intervalLength;
         }
-        err = (max - min) / intervalLength;
-      }
       return Convert.ToString(x);
     }
 
     public static double EquationGet(double x){
-      return Math.Cos(x) - x;
+      return Math.Cos(x) + x;
     }
   }
 }

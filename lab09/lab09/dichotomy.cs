00e6a26606885b8e570db9798ca27080238bd83a
Maxim Vorobyev <vmaxim7777@gmail.com>
2020-03-06 12:49:29 +0300

    fix

--- lab09/lab09/dichotomy.cs
+++ lab09/lab09/dichotomy.cs
@@ -3,7 +3,7 @@
 namespace Dichotomy {
   public static class Dichotomy {
     public delegate double delegateEquation(double x);
-    private const double Eps = 1e-14;
+    private const double Eps = 1e-15;
 
     public static string DichotomySolve(double borderFirst, double borderSecond, delegateEquation GetEq, double accuracy) {
       var min = borderFirst;
@@ -12,19 +12,15 @@ namespace Dichotomy {
       var err = intervalLength;
       var x = 0.0;
 
-        while (err > accuracy && GetEq(x) != 0) {
-          x = (min + max) / 2;
-          if (GetEq(min) * GetEq(x) < 0) {
-            max = x;
-          } else if (GetEq(x) * GetEq(max) < 0) {
-            min = x;
-          } else if (GetEq(min) * GetEq(max) < 0) {
-            Console.WriteLine("ok");
-          } else {
-            Console.WriteLine("kek");
-          }
-          err = (max - min) / intervalLength;
-        }
+      while (err > accuracy && GetEq(x) != Eps) {
+        x = (min + max) / 2;
+        if (GetEq(min) * GetEq(x) < Eps) {
+          max = x;
+        } else if (GetEq(x) * GetEq(max) < Eps) {
+          min = x;
+        } 
+        err = (max - min) / intervalLength;
+      }
       return Convert.ToString(x);
     }
 

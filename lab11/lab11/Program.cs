a4ce903770b7f60f0d7127e23c8f4e02401d2060
Maxim Vorobyev <vmaxim7777@gmail.com>
2020-03-15 12:41:28 +0300

    add dynamic

--- lab11/lab11/Program.cs
+++ lab11/lab11/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using ComplexOp;
 using EventCustomOp;
+using VectorOp;
 
 namespace lab11 {
   class Program {
     static void Main(string[] args) {
-      var A = new ComplexNum(-4, 0);
-      var B = new ComplexNum(0, 0);
-      ComplexNum division = new ComplexNum();
-      ComplexNum.DivisionZero += ComplexNum.ShowMessage;
-      Console.WriteLine((A / B).ToString());
+      var A = new ComplexNum(1.0, 2.0);
+      var B = new ComplexNum(1.5, 3.0);
+
+      //подписываем метод на событие
+      //ComplexNum division = new ComplexNum();
+      //ComplexNum.DivisionZero += ComplexNum.ShowMessage;
+      //Console.WriteLine((A / B).ToString());
+
+      Vector<ComplexNum> complexVector1 = new Vector<ComplexNum>(A, B);
+      Vector<ComplexNum> complexVector2 = new Vector<ComplexNum>(B, A);
+      Console.WriteLine(Vector<ComplexNum>.VectorSum(complexVector1, complexVector2).ToString());
     }
   }
 }
---
+++ lab11/lab11/Vector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VectorOp {
+  class Vector<T> {
+    public T _x;
+    public T _y;
+
+    public Vector() { }
+
+    public Vector(T x, T y) {
+      this._x = x;
+      this._y = y;
+    }
+
+    //сумма
+    public static Vector<T> VectorSum(Vector<T> A, Vector<T> B) {
+      var resultVector = new Vector<T>();
+      resultVector._x = (T)Convert.ChangeType((dynamic)A._x + (dynamic)B._x, typeof(T));
+      resultVector._y = (T)Convert.ChangeType((dynamic)A._y + (dynamic)B._y, typeof(T));
+      return resultVector;
+    }
+
+    //вывод
+    public override string ToString() {
+      return "vector(" + this._x + ",  " + this._y + ")";
+    }
+  }
+}

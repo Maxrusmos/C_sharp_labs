f8c13e02ac9dcc9707da2ed134dc45ed6969da45
Maxim Vorobyev <vmaxim7777@gmail.com>
2020-04-04 17:38:16 +0300

    default

--- lab13/lab13/Program.cs
+++ lab13/lab13/Program.cs
@@ -1,11 +1,47 @@
 ﻿using System;
 using System.IO;
 using SearchFilesOp;
+using System.IO.Compression;
 
 namespace lab13 {
   class Program {
     static void Main(string[] args) {
-      SearchFilesOp.SearchFiles.FileSearch();
+      //получаем переменную Windows с адресом текущего пользователя
+      string PatchProfile = Environment.GetEnvironmentVariable("USERPROFILE");
+      //ищем все вложенные папки 
+      string[] S = SearchDirectory(PatchProfile);
+      //создаем строку в которой соберем все пути
+      string ListPatch = "найденные файлы \n"; //заголовок для строк
+      foreach (string folderPatch in S) {
+        //добавляем новую строку в список
+        try {
+          //пытаемся найти данные в папке 
+          string[] F = SearchFile(folderPatch, "*.png");
+          foreach (string FF in F) {
+            //добавляем файл в список 
+            ListPatch += FF + "\n";
+          }
+        }
+        catch {
+        }
+      }
+      //выводим список на экран 
+      MessageBox.Show(ListPatch);
+      Console.WriteLine(ListPatch);
+    }
+
+    static string[] SearchDirectory(string patch) {
+      //находим все папки в по указанному пути
+      string[] ReultSearch = Directory.GetDirectories(patch);
+      //возвращаем список директорий
+      return ReultSearch;
+    }
+
+    static string[] SearchFile(string patch, string pattern) {
+      /*флаг SearchOption.AllDirectories означает искать во всех вложенных папках*/
+      string[] ReultSearch = Directory.GetFiles(patch, pattern, SearchOption.AllDirectories);
+      //возвращаем список найденных файлов соответствующих условию поиска 
+      return ReultSearch;
     }
   }
 }
--- lab13/lab13/SearchFiles.cs
+++ lab13/lab13/SearchFiles.cs
@@ -1,30 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO.Compression;
 using System.IO;
 
 namespace SearchFilesOp {
   class SearchFiles {
     public static void FileSearch() {
-      string catalog = @"C:\Users\vmaxi\Desktop\3_6sem\РПКС\labs";
-
+      string catalog = @"C:\Users\vmaxi\Desktop\";
       string fileName = "Файл.txt";
-
-      //проводим поиск в выбранном каталоге и во всех его подкаталогах
-      foreach (string findedFile in Directory.EnumerateFiles(catalog, fileName,
-          SearchOption.AllDirectories)) {
+      var fileNameCompressed = catalog + fileName.Split('.')[0] + ".txt";
+      foreach (string findedFile in Directory.EnumerateFiles(catalog, fileName, SearchOption.AllDirectories)) {
         FileInfo FI;
         try {
-          //по полному пути к файлу создаём объект класса FileInfo
           FI = new FileInfo(findedFile);
-          //найденный результат выводим в консоль (имя, путь, размер, дата создания файла)
-          Console.WriteLine(FI.Name + " " + FI.FullName + " " + FI.Length + "_байт" +
-                              " Создан: " + FI.CreationTime);
-
+          Console.WriteLine(FI.Name + " " + FI.FullName + " " + FI.Length + "_байт" + " Создан: " + FI.CreationTime);
+          Compress(FI.FullName, fileNameCompressed);
         }
-        catch //слишком длинное имя файла
-        {
+        catch {
           continue;
+        }
+      }
+    }
+
+    public static void Compress(string sourceFile, string compressedFile) {
+      using (FileStream sourceStream = new FileStream(sourceFile, FileMode.OpenOrCreate)) {
+        using (FileStream targetStream = File.Create(compressedFile)) {
+          using (GZipStream compressionStream = new GZipStream(targetStream, CompressionMode.Compress)) {
+            sourceStream.CopyTo(compressionStream);
+            Console.WriteLine("Сжатие файла {0} завершено. Исходный размер: {1}  сжатый размер: {2}.",
+                sourceFile, sourceStream.Length.ToString(), targetStream.Length.ToString());
+          }
         }
       }
     }

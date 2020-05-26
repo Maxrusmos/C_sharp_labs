d2a84f44fd54a0adad873375aa8b6e319fd8d948
Maxim Vorobyev <vmaxim7777@gmail.com>
2020-05-26 16:35:29 +0300

    ForRoma

---
+++ Roma/Roma/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Roma {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine(sknfSdnfFromFile.Builder().ToString());   
+    }
+  }
+}
